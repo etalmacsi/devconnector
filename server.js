abb14393e7aa9677a3eac47dccdf5b2ca1dcfaf4
etalmacsi <eddyedvin@gmail.com>
2018-11-03 17:10:06 +0100

    Add passport and current user route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //DB config
+
 const db = require("./config/keys").mongoURI;
 
 //connect to mongoDB
@@ -21,7 +23,13 @@ mongoose
   .then(() => console.log("mongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+// middleware passport
+
+app.use(passport.initialize());
+
+//passport config
+
+require("./config/passport")(passport);
 
 //use routes
 
