1f21bc9ec9534d1ec37cf37d7bca6b2d3d67c02c
Unknown <etalmacsi@monguz.hu>
2019-02-09 20:26:20 +0100

    Add post model

--- models/post.js
+++ models/post.js
@@ -4,8 +4,54 @@ const Schema = mongoose.Schema;
 //create schema
 
 const PostSchema = new Schema({
-    user:{
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
         type: Schema.Types.ObjectId,
-        ref:    
+        ref: "users"
+      }
     }
-})
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
