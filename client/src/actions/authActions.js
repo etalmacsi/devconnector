01ac800af0c55105571168dc869ae50c59d347ae
Unknown <etalmacsi@monguz.hu>
2019-03-24 14:14:53 +0100

    Login redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,9 @@
-import { GET_ERRORS } from "./types";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS } from "./types";
+import { SET_CURRENT_USER } from "./types";
 
 // register user
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +16,37 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login  - Get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .pst("/api/users/login", userData)
+    .then(res => {
+      //save to local Storage
+      const { token } = res.data;
+      //set token to localstorage
+      localStorage.setItem("jwtToken", token);
+      // set token to auth header
+      setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      //Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set logged in user
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
