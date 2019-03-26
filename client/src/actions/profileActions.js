98c31d9100cb0359e7bae05e334e8cf2480d66a7
Unknown <etalmacsi@monguz.hu>
2019-03-26 19:47:54 +0100

    Display Experience and delete function

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -81,6 +81,25 @@ export const addEducation = (eduData, history) => dispatch => {
     );
 };
 
+//Delete experience
+
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 //delete account and profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This can Not be undo!")) {
