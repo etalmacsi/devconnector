58922f8c793de184b6b7e355e9f627138b55c675
etalmacsi <eddyedvin@gmail.com>
2019-03-30 15:24:47 +0100

    Add profile components

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credantials/AddExperience";
 import AddEducation from "./components/add-credantials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 
 import "./App.css";
 
@@ -58,6 +59,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import ProfileHeader from "./ProfileHeader";
+import ProfileAbout from "./ProfileAbout";
+import ProfileCreds from "./ProfileCreds";
+import ProfileGithub from "./ProfileGithub";
+import Spinner from "../common/Spinner";
+import { getProfilebyHandle } from "../../actions/profileActions";
 
 class Profile extends Component {
+  ComponentDidMound() {
+    if (this.props.match.params.handle) {
+      this.props.getProfilebyHandle(this.props.match.params.handle);
+    }
+  }
   render() {
-    return <div />;
+    return (
+      <div>
+        <ProfileHeader />
+        <ProfileAbout />
+        <ProfileCreds />
+        <ProfileGithub />
+      </div>
+    );
   }
 }
 
-export default Profile;
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  getProfilebyHandle: PropTypes.func.isRequired
+};
+
+const mapstateToProps = state => ({
+  profle: state.profile
+});
+
+export default connect(
+  mapstateToProps,
+  { getProfilebyHandle }
+)(Profile);
