9dfc766f0a592702d6aaefdd26640ac6ee3e40ff
Unknown <etalmacsi@monguz.hu>
2019-04-02 23:07:23 +0200

    Post a comment

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -90,3 +90,16 @@ export const getPost = id => dispatch => {
     )
     .catch(err => dispatch({ type: GET_POST, payload: null }));
 };
+
+//Add comment
+export const addComment = (postId, commentData) => dispatch => {
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
