df7b3ead2803f28a8dd6483a244e25d4bea9b0eb
Unknown <etalmacsi@monguz.hu>
2019-04-02 23:28:55 +0200

    Add Delete and displayComment

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -5,11 +5,13 @@ import {
   GET_ERRORS,
   POST_LOADING,
   GET_POST,
-  DELETE_POST
+  DELETE_POST,
+  CLEAR_ERRORS
 } from "./types";
 
 // Add post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -37,10 +39,17 @@ export const getPosts = () => dispatch => {
 
 //Set loading state
 
-// Add post
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  };
+};
+
+//Clear errors
+
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   };
 };
 
@@ -93,8 +102,22 @@ export const getPost = id => dispatch => {
 
 //Add comment
 export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
+//Delete comment
+export const deleteComment = (postId, commenId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commenId}`)
     .then(res =>
       dispatch({
         type: GET_POST,
